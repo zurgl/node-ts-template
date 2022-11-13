b8503bd2cf15fbe427c0e9ba68dcc74fa5a384e2
zurgl <elayar.yacine@gmail.com>
2022-11-13 16:39:28 +0100

    refine

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello world!')
+import parseUrl from 'parse-url'
+
+import { pipe } from 'fp-ts/lib/function'
+import * as O from 'fp-ts/lib/Option'
+import * as A from 'fp-ts/lib/Array'
+
+type ParsedUrl = ReturnType<typeof parseUrl>
+
+const url = 'http://www.domain.com/path/name?foo=bar&bar=42#some-hash'
+
+function tryParsingUrl(url: string): O.Option<ParsedUrl> {
+  return O.tryCatch(() => parseUrl(url))
+}
+
+function getTld(resource: string): O.Option<string> {
+  return pipe(resource, (r) => r.split('.'), A.last)
+}
+
+function isDotComOpt(url: string): boolean {
+  return pipe(
+    url,
+    tryParsingUrl,
+    O.map((p) => p.resource),
+    O.chain(getTld),
+    O.exists((tld) => tld == 'com')
+  )
+}
+
+console.log(isDotComOpt(url))
